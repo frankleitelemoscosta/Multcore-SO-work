10abd5a2151d8aa76f6f3f392b243b8d7a8f8dc3
Getulio-Mendes <mendes.getulio@outlook.com>
2025-01-06 22:30:26 -0300

    No assembly on git

---
+++ src/assembler/assembler.h
@@ -0,0 +1,16 @@
+#ifndef ASSEMBLER_H
+#define ASSEMBLER_H
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+#include <bitset>
+#include <algorithm>
+
+using namespace std;
+int assembleFiles(int count, vector<string> files, char* filePaths[]);
+
+#endif
